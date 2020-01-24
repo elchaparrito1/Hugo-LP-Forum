e382777aa77ad91b3aff5c40797697d48bb1028c
Mitchell Waite <mitchellwaite@Aubreys-MacBook-Pro.local>
2020-01-23 19:14:07 -0700

    beta version of site

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,71 @@
-import useSWR from 'swr';
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
 
-function fetcher(url) {
-  return fetch(url).then(r => r.json());
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ];
 }
 
-export default function Index() {
-  const { data, error } = useSWR('/api/randomQuote', fetcher);
-  // The following line has optional chaining, added in Next.js v9.1.5,
-  // is the same as `data && data.author`
-  const author = data?.author;
-  let quote = data?.quote;
-
-  if (!data) quote = 'Loading...';
-  if (error) quote = 'Failed to fetch the quote.';
-
-  return (
-    <main className="center">
-      <div className="quote">{quote}</div>
-      {author && <span className="author">- {author}</span>}
-
+const PostLink = ({ post }) => (
+    <li>
+      <Link href="/p/[id]" as={`/p/${post.id}`}>
+        <a>{post.title}</a>
+      </Link>
       <style jsx>{`
-        main {
-          width: 90%;
-          max-width: 900px;
-          margin: 300px auto;
-          text-align: center;
+        li {
+          list-style: none;
+          margin: 5px 0;
         }
-        .quote {
-          font-family: cursive;
-          color: #e243de;
-          font-size: 24px;
-          padding-bottom: 10px;
+  
+        a {
+          text-decoration: none;
+          color: blue;
+          font-family: 'Arial';
         }
-        .author {
-          font-family: sans-serif;
-          color: #559834;
-          font-size: 20px;
+  
+        a:hover {
+          opacity: 0.6;
         }
       `}</style>
-    </main>
+    </li>
+  );
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        {getPosts().map(post => (
+          <PostLink key={post.id} post={post} />
+        ))}
+      </ul>
+      <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+    </Layout>
   );
 }
